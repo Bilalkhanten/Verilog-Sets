3310f4fd8643f27a9903c3c4f455284b69ea8574
Bilal Khan <Bilalkhanten@gmail.com>
2017-08-27 04:53:57 -0700

    Create RSFF.v

---
+++ RSFF.v
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+// Engineer: Bilal khan                                                        //
+// Create Date:    04/15/2016                                                  //
+// Module Name:    RSFF_Behavioral                                             //
+// Description:    Behavioral Model of RS Flip Flop                            //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 10ps
+module RSFF(RS,clk,q,qbar);
+  input [1:0] RS;
+  output q,qbar;
+  input clk;
+  reg q,qbar;
+  
+  always @(posedge clk) begin
+    case (RS)
+      2'd0: q = q;
+      2'd1: q = 0;
+      2'd2: q = 1;
+      2'd3: q = 1'bx;
+    endcase
+    qbar = ~q;
+  end
+endmodule
